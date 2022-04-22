a59e9aba934905507ab948266009bba52f32dc83
Richard L Zarth III <rlziii@icloud.com>
2022-04-21 22:24:52 -0400

    Initial commit.

---
+++ Nikki/NikkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikkiApp.swift
+//  Nikki
+//
+//  Created by Richard L Zarth III on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NikkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
