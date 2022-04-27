0a465a4c5f83adae20192bdb3f5bd59d77c42260
Richard L Zarth III <rlziii@icloud.com>
2022-04-26 22:15:23 -0400

    Setup SettingsView.

--- Nikki/User Interface/Settings/SettingsView.swift
+++ Nikki/User Interface/Settings/SettingsView.swift
@@ -1,9 +1,64 @@
 import SwiftUI
 
+class SettingsViewModel: ObservableObject {
+    @Published var simpleRowsEnabled = false
+    @Published var defaultAuthorName = ""
+
+    let appURL = URL(string: "https://github.com/rlziii/Nikki")!
+    let versionNumber = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
+    let buildNumber = Bundle.main.infoDictionary!["CFBundleVersion"] as! String
+}
+
 struct SettingsView: View {
+    @StateObject private var viewModel = SettingsViewModel()
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
-        Text("Hello, World!")
-            .navigationTitle(Text("Settings"))
+        Form {
+            Section {
+                TextField("Default Author Name", text: $viewModel.defaultAuthorName)
+            } header: {
+                Text("Author Info")
+            }
+
+            Section {
+                Toggle("Simple Rows", isOn: $viewModel.simpleRowsEnabled)
+            } header: {
+                Text("App Settings")
+            }
+
+            Section {
+                Link("GitHub", destination: viewModel.appURL)
+            } header: {
+                Text("App Info")
+            } footer: {
+                versionAndBuildInfoFooter()
+            }
+        }
+        .navigationTitle(Text("Settings"))
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .cancellationAction) {
+                Button("Cancel", action: { dismiss() })
+                    .keyboardShortcut(.cancelAction)
+            }
+        }
+    }
+
+    func versionAndBuildInfoFooter() -> some View {
+        HStack {
+            HStack(spacing: 0) {
+                Text("Version number: ")
+                Text(verbatim: viewModel.versionNumber)
+            }
+
+            Spacer()
+
+            HStack(spacing: 0) {
+                Text("Build number: ")
+                Text(verbatim: viewModel.buildNumber)
+            }
+        }
     }
 }
 
